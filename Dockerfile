5b294c69eab9ed9b293e34d87f02cc117e61da91
Felix Dubrownik <fdubrownik@gmail.com>
2023-03-14 18:36:28 +0100

    feat: update gh actions. add logging to backend

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ FROM ghcr.io/getzola/zola:v0.17.1 AS fe_builder
 
 WORKDIR /app
 
-COPY --from=fe_installer /app/node_modules ./
+COPY --from=fe_installer /app/node_modules ./node_modules
 COPY frontend/ ./
 RUN ["/bin/zola", "build"]
 
@@ -26,6 +26,6 @@ COPY ./src ./src
 RUN cargo build --release
 
 FROM gcr.io/distroless/cc
-COPY --from=be_builder /app/target/release/felix-bause-dev /opt/
-COPY --from=fe_builder /app/public /opt/
-ENTRYPOINT [ "/opt/felix-bause-dev" ]
+COPY --from=be_builder /app/target/release/felix-bause-dev /felix-bause-dev
+COPY --from=fe_builder /app/public /public
+ENTRYPOINT [ "/felix-bause-dev" ]
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,26 @@ use axum::{
 };
 
 use tower_http::{
-    services::{ServeDir}
+    services::{ServeDir},
+    trace::TraceLayer,
 };
+use tracing::Span;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "felix_bause_dev=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     // build our application with a single route
-    let app = Router::new().nest_service("/", ServeDir::new("public"));
+    let app = Router::new()
+        .nest_service("/", ServeDir::new("public"))
+        .layer(TraceLayer::new_for_http());
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
